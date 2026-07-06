ed7b65d500c09910b131ff7e7c478aeca8d7a205
Paul1l <pauletolava1804@gmail.com>
2026-07-07 01:20:36 +0700

    Add MB Kuzbass site

---
+++ mb-kuzbass/reviews-config.js
@@ -0,0 +1,8 @@
+window.MBKUZBASS_REVIEWS_CONFIG = {
+  branchId: '70000001084067232',
+  endpoint: 'https://catalog.api.2gis.com/3.0/items/byid',
+  fields: 'items.reviews',
+  locale: 'ru_RU',
+  apiKey: '',
+  proxyUrl: '',
+};
